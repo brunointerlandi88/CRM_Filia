294b5306a8e0055b7f65543596fb664393a5da11
Bruno Interlandi <brunointerlandi88@yahoo.it>
2021-05-18 08:23:13 +0200

    Update settings.py

--- newsite/settings.py
+++ newsite/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -83,7 +82,7 @@ WSGI_APPLICATION = 'newsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
          'USER': 'bruno',
          'PASSWORD': 'bruno',
     }
